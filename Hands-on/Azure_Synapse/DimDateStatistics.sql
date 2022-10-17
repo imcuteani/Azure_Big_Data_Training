fcefd05d7cd2c9b3b7c381d59dc5d7e6040ff603
Anindita Basak <anindita.basak@live.com>
2022-10-17 17:48:51 +0530

    Create DimDateStatistics.sql

---
+++ Hands-on/Azure_Synapse/DimDateStatistics.sql
@@ -0,0 +1,3 @@
+CREATE STATISTICS [DateId] on [DimDate2] ([DateId]);
+CREATE STATISTICS [CalendarQuarter] on [DimDate2] ([CalendarQuarter]);
+CREATE STATISTICS [FiscalQuarter] on [DimDate2] ([FiscalQuarter]);
