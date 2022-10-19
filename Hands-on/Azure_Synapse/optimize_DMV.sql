fa0cd492f7bd5f808e4d1588fe61805604130291
Anindita Basak <anindita.basak@live.com>
2022-10-19 11:34:55 +0530

    Create optimize_DMV.sql

---
+++ Hands-on/Azure_Synapse/optimize_DMV.sql
@@ -0,0 +1,30 @@
+-- To see all requests
+SELECT * FROM sys.dm_pdw_exec_requests;
+
+-- To see a particular request identified by its label
+SELECT * FROM sys.dm_pdw_exec_requests as r
+WHERE r.[label] = 'CTAS : Load [cso].[DimProduct]             '
+      OR r.[label] = 'CTAS : Load [cso].[FactOnlineSales]        '
+;
+
+-- To track bytes and files
+SELECT
+    r.command,
+    s.request_id,
+    r.status,
+    count(distinct input_name) as nbr_files,
+    sum(s.bytes_processed)/1024/1024/1024 as gb_processed
+FROM
+    sys.dm_pdw_exec_requests r
+    inner join sys.dm_pdw_dms_external_work s
+        on r.request_id = s.request_id
+WHERE
+    r.[label] = 'CTAS : Load [cso].[DimProduct]             '
+    OR r.[label] = 'CTAS : Load [cso].[FactOnlineSales]        '
+GROUP BY
+    r.command,
+    s.request_id,
+    r.status
+ORDER BY
+    nbr_files desc,
+    gb_processed desc;
