661e2f7cf9181ba76b4bce050292e1a1e5fd25fa
Anindita Basak <anindita.basak@live.com>
2022-10-19 11:33:51 +0530

    Create External_table.sql

---
+++ Hands-on/Azure_Synapse/External_table.sql
@@ -0,0 +1,9 @@
+CREATE EXTERNAL FILE FORMAT TextFileFormat
+WITH
+(   FORMAT_TYPE = DELIMITEDTEXT
+,    FORMAT_OPTIONS    (   FIELD_TERMINATOR = '|'
+                    ,    STRING_DELIMITER = ''
+                    ,    DATE_FORMAT         = 'yyyy-MM-dd HH:mm:ss.fff'
+                    ,    USE_TYPE_DEFAULT = FALSE
+                    )
+);
