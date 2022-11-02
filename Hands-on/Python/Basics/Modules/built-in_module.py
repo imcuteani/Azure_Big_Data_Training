9afcab09c3a605d1f71099fe62a893a9c3e7d16b
Anindita Basak <anindita.basak@live.com>
2022-11-02 09:50:30 +0530

    updated built-in_module.py

--- Hands-on/Python/Basics/Modules/built-in_module.py
+++ Hands-on/Python/Basics/Modules/built-in_module.py
@@ -9,15 +9,9 @@
 
 
 import os 
-os.mkdir('E:\Azure\Training\Capgemini\Assignments\Azure_Big_Data_Training\Hands-on\Python\module')
+os.mkdir('<your_working_directory>')
 os.getcwd()           # getting current working directory
+os.rmdir('<your_working_directory>')
 
 
-# Sys module 
 
-# The sys module provides functions and variables used to manipulate different parts of python runtime env.
-# sys.argv returns a list of arguments passed to a python script. the item in index 0 in the list is always the name of the script, 
-# at index 1 , refers to the argument passed from the command line
-
-import sys
-print('Welcome {}. Enjoy {} challenge!'.format(sys.argv[1], sys.argv[2]))
