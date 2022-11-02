c6cbed4e51290749fafbc2358c055d0e602fa745
Anindita Basak <anindita.basak@live.com>
2022-11-02 12:18:28 +0530

    added exception block pass

--- Hands-on/Python/Basics/Exception-Handling/type_error.py
+++ Hands-on/Python/Basics/Exception-Handling/type_error.py
@@ -16,3 +16,13 @@ else:
     print('I usually run this try block')
 finally: 
     print('This is the exception handling sample')                   
+
+# just passing exception block 
+try: 
+    name = input('Enter your name:')
+    year_born = input('Year_you_born:')
+    age = 2022 - int(year_born)
+    print('You are {name}. And your age is {age}.', age)
+
+except Exception as e:
+    print(e) 
