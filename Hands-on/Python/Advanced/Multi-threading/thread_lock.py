b36984884673d377c555fd9c1819fdacae4a9240
Anindita Basak <anindita.basak@live.com>
2022-11-03 12:59:30 +0530

    added thread lock acq and release in py

---
+++ Hands-on/Python/Advanced/Multi-threading/thread_lock.py
@@ -0,0 +1,40 @@
+# thread synchronization in python 
+
+import threading
+
+#global variable 
+x = 0 
+
+def increment_val():
+    global x
+    x += 1
+
+def thread_task(lock):
+    for _ in range(100000):      # calls thread_task upto 1M times
+        lock.acquire()
+        increment_val()
+        lock.release()
+        
+
+def main_task(): 
+    global x
+    x = 0 
+# Creating a lock on the thread to overcome the mutual exclusive scenario
+    lock = threading.Lock()
+
+    # Creating threads 
+    t1 = threading.Thread(target=thread_task, args=(lock,))
+    t2 = threading.Thread(target=thread_task, args=(lock,))
+
+    # start the threads
+    t1.start()
+    t2.start()
+
+    # wait for the threads to finish execution 
+    t1.join()
+    t2.join()
+
+if __name__ == "__main__": 
+    for i in range(10):
+        main_task()
+        print("Iteration for {0}: x = {1}" .format(i,x))   
